f0a51bdd1b1763d683252c8d0c6ed7d7c58ea344
npolyakova <natusj13@mail.ru>
2024-12-13 00:17:53 +0300

    Add suggests

--- src/app/main.py
+++ src/app/main.py
@@ -6,7 +6,6 @@ import psycopg2
 app = FastAPI()
 
 def get_categories():
-    conn = None
     try:
         conn = psycopg2.connect('postgresql://ml_user:pgpass@localhost:5432/mldb')
     except:
@@ -24,6 +23,20 @@ def get_categories():
 
     return pre_json
 
+def get_suggest(category1: str, category2: str):
+    try:
+        conn = psycopg2.connect('postgresql://ml_user:pgpass@localhost:5432/mldb')
+    except:
+        print('Can`t establish connection to database')
+
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT next_category FROM categories WHERE category1 = {category1} AND category2 = {category2}')
+    next_cat = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    return {"id": next_cat[0], "name": next_cat[1]}
+
 @app.get("/")
 def read_root():
     return {"message" : "This is the api for education ML project"}
@@ -32,4 +45,9 @@ def read_root():
 def get_all_categories():
     cat_dict = {"categories": get_categories()}
     json_compatible_item_data = jsonable_encoder(cat_dict)
+    return JSONResponse(content=json_compatible_item_data)
+
+@app.get("/suggest")
+def get_model(category1, category2):
+    json_compatible_item_data = jsonable_encoder(get_suggest(category1, category2))
     return JSONResponse(content=json_compatible_item_data)
